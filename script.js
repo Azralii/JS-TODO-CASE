71a0e7fc4e4bd97488ffa966526a56058e2efc89
Azralii <111491491+Azralii@users.noreply.github.com>
2022-11-28 19:30:45 +0100

    har ändrat

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("addNewList").onclick = function(outputName){ 
-    var dish = prompt("Enter the Dish Name");
-    var li = document.createElement("li");
-    li.innerText = dish;
-    document.getElementById("outputName").innerText = dish;
-
-}

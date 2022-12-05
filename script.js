5d197d9b8cef6d90e5d6876127960bb489d532b3
Azralii <111491491+Azralii@users.noreply.github.com>
2022-12-05 18:48:04 +0100

    jag har korrigerat css ocg javascript

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const inputText = document.querySelector("#txt");
 const myButton = document.querySelector(".btn-list");
 const list = document.querySelector(".container ul");
-myButton.addEventListener("click",(e)=> {
+myButton.addEventListener("click",(e)=>{  
     if(inputText.value != ""){
 
     e.preventDefault();
     // skapa list
     const myLi = document.createElement("li");
+    myLi.className = "checked";
     myLi.innerHTML = inputText.value;
     list.appendChild(myLi);
     //skapa span
@@ -18,6 +19,15 @@ myButton.addEventListener("click",(e)=> {
     for(let i=0;i<close.length;i++){
         close[i].addEventListener("click", ()=>{
             close[i].parentElement.style.display = "none";
+        })
+    }
+    const mark = document.querySelectorAll("li");
+    for(let i=0;i<mark.length;i++){
+        mark[i].addEventListener("click",() =>{
+            mark[i].parentElement.style.color = "green";
+            mark[i].parentElement.style.borderColor = "white";
+            mark[i].parentElement.style.borderStyle = "solid";
+            mark[i].parentElement.style.borderWidth = "0 2px 2px 0";
         })
     }
     inputText.value = "";

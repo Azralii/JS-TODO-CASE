3439f69cc0a1204b02c47d5dd174b737d67d7efe
Azralii <111491491+Azralii@users.noreply.github.com>
2022-12-09 15:20:58 +0100

    det är nästan klar

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-const list = document.querySelector(".container ul");
-myButton.addEventListener("click",(e)=>{  
-    if(inputText.value != ""){
+const inputText = document.getElementById("txt"),
+              items = document.querySelectorAll("#list li");
+        var tab = [], index;
+         // get the selected li index using array
+         //populate array with li values
 
-    e.preventDefault();
-    // skapa list
-    const myLi = document.createElement("li");
-    myLi.className = "checked";
-    myLi.innerHTML = inputText.value;
-    list.appendChild(myLi);
-    //skapa span
-    const mySpan = document.createElement("span");
-    mySpan.innerHTML = "X";
-    myLi.appendChild(mySpan);
-}
-    const close = document.querySelectorAll("span");
-    for(let i=0;i<close.length;i++){
-        close[i].addEventListener("click", ()=>{
-            close[i].parentElement.style.display = "none";
-        })
+        for(var  i=0; i<items.length; i++){
+            tab.push(items[i].innerHTML);
+             }
+
+        //get li index onclick     
+            for(var i=0; i<items.length; i++){
+            items[i].onclick = function(){
+                index = tab.indexOf(this.innerHTML);
+                console.log(this.innerHTML + "Index = " + index);
+                inputText.value = this.innerHTML;
+            };
+        }
+        function addItem(){
+            const listNode = document.getElementById("list");
+            const idItem = document.getElementById("item");
+            const textNode = document.createTextNode(inputText.value);
+            const liNode = document.createElement("li");
+            liNode.appendChild(textNode);
+            listNode.appendChild(liNode);
+            const markButton = document.createElement("button"),
+            delButton = document.createElement("button");
+            markButton.innerHTML = "Mark";
+            delButton.innerHTML = "Delete";
+            listNode.appendChild(markButton);
+            listNode.appendChild(delButton);
+            document.getElementById("list").appendChild(liNode);
+            delButton.addEventListener("click",()=>{
+            removeItem();
+        });
+            markButton.addEventListener("click",()=>{
+            markItem();
+        });
+                 }
+        function removeItem() { 
+            
+                items[index].parentNode.removeChild(items[index]);
+        
+        }
+        function markItem(){
+            
+            
+               
+                for(var i=0; i<items.length; i++){
+                    items[i].parentElement.style.backgroundColor = "blue";
+        }
     }
-    const mark = document.querySelectorAll("li");
-    for(let i=0;i<mark.length;i++){
-        mark[i].addEventListener("click",() =>{
-            mark[i].parentElement.style.color = "green";
-            mark[i].parentElement.style.borderColor = "white";
-            mark[i].parentElement.style.borderStyle = "solid";
-            mark[i].parentElement.style.borderWidth = "0 2px 2px 0";
-        })
-    }
-    inputText.value = "";
-});
+        function editItem(){
+            items[index].innerHTML = inputText.value;
+        }
